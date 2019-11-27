34bec91a576554e408e644ad7320979734949f26
Amantina Sosa <amantinasosa@gmail.com>
2019-11-26 20:48:41 -0500

    cycle-2 complete

--- browser/components/NewStudentForm.js
+++ browser/components/NewStudentForm.js
@@ -1,22 +1,65 @@
 import React, { Component } from 'react';
 
 export default class NewStudentForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    //update the state according to the form input
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    //will take the information & clear the state
+    event.preventDefault();
+    this.setState({
+      firstName: '',
+      lastName: '',
+      email: '',
+    });
+    // alert('a name was submitted:' + this.state.firstName);
+  }
   render() {
+    console.log('console log from new student form', this.state);
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           First Name:
-          <input type="text" name="firstName" />
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+          />
         </label>
 
         <label>
           Last Name:
-          <input type="text" name="lastName" />
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+          />
         </label>
 
         <label>
           Email:
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
         </label>
 
         <button type="submit">Submit New Student</button>
